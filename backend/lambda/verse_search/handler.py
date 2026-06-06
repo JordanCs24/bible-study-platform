529a422240a4d4acd63bff5f6032350dd9f4c94b
Jordan Sowell <155126975+JordanCs24@users.noreply.github.com>
2026-06-06 12:42:47 -0400

    TO DO: Write the logic for the Parse function to handle edge cases for Edge one cause users can be very unpredictable.

--- backend/lambda/verse_search/handler.py
+++ backend/lambda/verse_search/handler.py
@@ -2,7 +2,7 @@ import json    # read and parse the Bible JSON files from S3
 import re      # detect query types using pattern matching
 import boto3   # talk to AWS services like S3 and Bedrock
 
-verse_pattern = re.compile(r'^[\d\s]*[a-zA-Z]+[\s\w]*\d+:\d+$')
+verse_pattern = re.compile(r'^[\d\s]*[a-zA-Z]+[\s\w]*\d+:\d+$') #Type 1 check
 
 BUCKET_NAME = 'bible-platform-jordan'
 
@@ -15,6 +15,16 @@ def normalize_query(query):
     query = re.sub(r'([a-zA-Z])(\d)', r'\1 \2', query)
     query = re.sub(r'(\d)([a-zA-Z])', r'\1 \2', query)
     return query.title()
+#TO DO: Write the logic for the this function
+def parse_verse_reference(query):
+    # your comment outline here
+    #Parse it to check to see if the
+    #John 3:16          → single verse
+    #John 3:16-20       → verse range
+    #John 3             → first 5 verses of chapter
+    #1 John 3:16        → books with number prefix
+    #john3:16           → no space, handled by normalization
+    #JOHN 3:16          → caps, handled by normalization
 
 # Step 1: Receive the event from API Gateway and extract the query string
 def lambda_handler(event, context):
@@ -41,26 +51,22 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps({'type': 'verse', 'query': query})
         }
+    #   look up the specific scripture directly from the S3 data file
+    # elif the query is a single word with no spaces
+    #   return the most well known verses containing that word via Bedrock
     elif ' ' not in query:
         print("Type 2: Single keyword")
         return {
             'statusCode': 200,
             'body': json.dumps({'type': 'keyword', 'query': query})
-        }
+        }   
+     #   send to Bedrock for a warm thoughtful conversational response
     else:
         print("Type 3: Conversational question")
         return {
             'statusCode': 200,
             'body': json.dumps({'type': 'conversational', 'query': query})
         }
-
-    
-    #   look up the specific scripture directly from the S3 data file
-    # elif the query is a single word with no spaces
-    #   return the most well known verses containing that word via Bedrock
-    # else
-    #   send to Bedrock for a warm thoughtful conversational response
-
 # Step 4: Format the result into a clean response
 # Step 5: Return the response back to API Gateway
 
