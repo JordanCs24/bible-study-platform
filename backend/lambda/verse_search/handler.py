a7966bb5adbf62716a1688bb6c534aa7659c3abc
Jordan Sowell <155126975+JordanCs24@users.noreply.github.com>
2026-06-06 11:09:51 -0400

    Testing for the Lambda function is good, added the rest of the logic for the lambda function
    For the gitignore added the venv so the python can have run in it's virtual environment.

--- backend/lambda/verse_search/handler.py
+++ backend/lambda/verse_search/handler.py
@@ -2,36 +2,49 @@ import json    # read and parse the Bible JSON files from S3
 import re      # detect query types using pattern matching
 import boto3   # talk to AWS services like S3 and Bedrock
 
+verse_pattern = re.compile(r'^[\d\s]*[a-zA-Z]+[\s\w]*\d+:\d+$')
+
 BUCKET_NAME = 'bible-platform-jordan'
 
 KJV_PREFIX = 'bible-data/kjv/'
 
-#Listing of query types
-qType1 = None
-qType2 = None
-qType3 = None #Send it to AWS bedrock 
 
 # Step 1: Receive the event from API Gateway and extract the query string
 def lambda_handler(event, context):
-    query = event.get('q')
-    if( len(query) < 4):
+    query = event["queryStringParameters"].get('q')
+    # Step 2 Validation: if the query is empty or less than 3 characters:
+    #   return a message saying "please enter a more specific search"
+    if(query is None):
+        print("Your search field is empty")
+    elif(len(query) < 3):
         print("please enter a more specific search")
-    elif(query)
+    # Step 3: Detect the query type and route accordingl
+    # if the query matches the verse reference pattern (book name + colon + numbers)
+    elif (re.match(verse_pattern, query)):
+        print("Type 1: Direct verse reference")
+        return {
+        'statusCode': 200,
+        'body': json.dumps({'type': 'verse', 'query': query})
+        }
+    elif ' ' not in query:
+        print("Type 2: Single keyword")
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'type': 'keyword', 'query': query})
+        }
     else:
-        Type3 = True
-        
-        
-# Step 2 Validation: if the query is empty or less than 3 characters:
-#   return a message saying "please enter a more specific search"
+        print("Type 3: Conversational question")
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'type': 'keyword', 'query': query})
+        }
 
-# Step 3: Detect the query type and route accordingly
-
-# if the query matches the verse reference pattern (book name + colon + numbers)
-#   look up the specific scripture directly from the S3 data file
-# elif the query is a single word with no spaces
-#   return the most well known verses containing that word via Bedrock
-# else
-#   send to Bedrock for a warm thoughtful conversational response
+    
+    #   look up the specific scripture directly from the S3 data file
+    # elif the query is a single word with no spaces
+    #   return the most well known verses containing that word via Bedrock
+    # else
+    #   send to Bedrock for a warm thoughtful conversational response
 
 # Step 4: Format the result into a clean response
 # Step 5: Return the response back to API Gateway
